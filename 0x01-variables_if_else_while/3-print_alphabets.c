9b38cd4b16f7268cc3512163323067590b376612
Nath126 <kolawolenathaniel26@gmail.com>
2022-09-09 06:41:55 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,9 +14,9 @@ int main(void)
 		   putchar(lower_case);
 	   }
 	   for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	   {
+	   	{
 		   putchar(upper_case);
-	   }
+	   	}
 	   putchar ('\n');
 	       return (0);
-} 
+}
